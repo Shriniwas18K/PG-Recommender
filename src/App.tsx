fbb0a4d84caf31a935f97ab089900cd8f6d51508
Shriniwas18K <kshriniwas180205@gmail.com>
2024-03-14 12:03:41 +0530

    initialized repo and configured dependencies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [token, setToken] =useState<string>('');
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
