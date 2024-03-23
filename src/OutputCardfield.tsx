693b50766403856ed38d6c969ecf94455631f62d
Shriniwas18K <kshriniwas180205@gmail.com>
2024-03-23 18:28:33 +0530

    merge

--- src/OutputCardfield.tsx
+++ src/OutputCardfield.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default function OutputCardfield(props :any){
     return(<>
         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -6,13 +5,4 @@ export default function OutputCardfield(props :any){
         <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{props.value}</dd>
     </div>
         </>)
-=======
-export default function OutputCardfield(props :any){
-    return(<>
-        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-        <dt className="text-sm font-medium leading-6 text-gray-900">{props.attr}</dt>
-        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{props.value}</dd>
-    </div>
-        </>)
->>>>>>> 8c3cffccacd48d9e1c2c8eeff9e8d860fd3b423b
 }
