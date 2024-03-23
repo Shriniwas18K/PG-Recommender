60d417873f1f50b982c7816b548d18fe6658f7e8
Abhishek Bhausaheb Gangurde <121115139+pccoeaiml@users.noreply.github.com>
2024-03-23 18:13:46 +0530

    Update Card.tsx

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,60 +1,31 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
-*/
-const products = [
-    {
-        id: 1,
-        name: 'Basic Tee',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-        imageAlt: "Front of men's Basic Tee in black.",
-        price: '$35',
-        color: 'Black',
-    },
-    // More products...
-]
-
+import OutputCardfield from "./OutputCardfield"
 export default function Cards() {
     return (
-        <div className="bg-white">
-            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
-
-                <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                    {products.map((product) => (
-                        <div key={product.id} className="group relative">
-                            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                                <img
-                                    src={product.imageSrc}
-                                    alt={product.imageAlt}
-                                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                                />
-                            </div>
-                            <div className="mt-4 flex justify-between">
-                                <div>
-                                    <h3 className="text-sm text-gray-700">
-                                        <a href={product.href}>
-                                            <span aria-hidden="true" className="absolute inset-0" />
-                                            {product.name}
-                                        </a>
-                                    </h3>
-                                    <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+        <div className="flex justify-center items-center h-screen">
+            <div className="max-w-4xl w-full px-6 py-8 bg-white shadow-md rounded-md" style={{ backgroundImage: 'url("http://www.pixelstalk.net/wp-content/uploads/2016/09/All-White-Background-HD.jpg")' }}>
+                <div className="mt-6 border-t border-gray-100">
+                    <dl className="divide-y divide-gray-100">
+                        <OutputCardfield attr="Owner name" value="output owner name" />
+                        <OutputCardfield attr="Address" value="output owner address" />
+                        <OutputCardfield attr="Owner phone number" value="output owner number" />
+                        <OutputCardfield attr="Rent per person" value="output rent" />
+                        <OutputCardfield attr="Area in Sqft" value="output area" />
+                        <OutputCardfield attr="Wifi Availability" value="output wifi" />
+                        <OutputCardfield attr="Pincode" value="output pincode" />
+                        <OutputCardfield attr="Furnished" value="output Furnished" />
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm font-medium leading-6 text-gray-900">Images</dt>
+                            <div className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                <div className="flex justify-center">
+                                    <div className="flex flex-wrap gap-4">
+                                        <img src="https://tse4.mm.bing.net/th?id=OIP.PYipJ_hSncugM2SwnZitvgHaEK&pid=Api&P=0&h=180" alt="Image 1" className="w-72 h-36 rounded-md" />
+                                        <img src="https://tse2.mm.bing.net/th?id=OIP.DpcLyyRCeTWoiiMNdCTXxQHaEK&pid=Api&P=0&h=180" alt="Image 2" className="w-72 h-36 rounded-md" />
+                                        <img src="https://tse4.mm.bing.net/th?id=OIP.1YM53mG10H_U25iPjop83QAAAA&pid=Api&P=0&h=180" alt="Image 3" className="w-72 h-36 rounded-md" />
+                                    </div>
                                 </div>
-                                <p className="text-sm font-medium text-gray-900">{product.price}</p>
                             </div>
                         </div>
-                    ))}
+                    </dl>
                 </div>
             </div>
         </div>
@@ -63,13 +34,13 @@ export default function Cards() {
 
 
 async function processData() {
-     fetch('https://smpg.onrender.com/retrieveProperties/412434').then(data=>{
+    fetch('https://smpg.onrender.com/retrieveProperties/412434').then(data => {
         return data.json()
-     }).then(data=>{
-        data.map(item=>{
+    }).then(data => {
+        data.map(item => {
             console.log(item)
         })
-     })
+    })
 }
 
 processData();
