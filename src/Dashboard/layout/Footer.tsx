a47196921a5f5a22c0dede544d85edce2ae3a660
Shriniwas Kulkarni <153389794+Shriniwas18K@users.noreply.github.com>
2024-09-12 23:48:12 +0530

    Delete src directory

--- src/Dashboard/layout/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer(){
-    return(<div className="w-full bg-slate-900 text-fuchsia-50 text-center p-7">
-            <p>Gharseek-Project</p>
-            <p>2024</p>
-            <p>Developed by</p>
-            <p>Abhishek Prajyot Shriniwas Pranav</p>
-        </div>)
-}
